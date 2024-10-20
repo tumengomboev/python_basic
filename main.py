5103ead6573d486f81ad0cc4e63b029262bc2847
Tumen Gomboev <tumengomboev88@gmail.com>
2024-10-20 09:31:12 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# print("hello, world!")
+# first = input("Type word:\n")
+# first = first.capitalize()
+# name = "alex"
+# name = name.capitalize()
+# phrase = f"{first}, {name}!"
+# print(phrase.upper())
+# print(name + first)
+
+a = input("Type word:\n")
+b = "guys"
+b = b.capitalize()
+c = f"{a}, {b}!"
+print(c)
+# print(c.upper())
+# print(a + b)
